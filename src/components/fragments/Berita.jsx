27ca266bc5b7326e3272110e1d879f60cb0e1b91
Denni <dennipes1@gmail.com>
2024-06-21 20:53:56 +0700

    make and update in berita

--- src/components/fragments/Berita.jsx
+++ src/components/fragments/Berita.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import berita from '../../assets/icons/berita.svg'
 import briLiga from '../../assets/liga/briLiga.png'
+import updateMV from '../../assets/update-mv.png'
 
 const Berita = () => {
   return (
-    <div className='bg-white mt-3'>
+    <div className='bg-white mt-3 pt-2 pb-2 pl-2'>
       <div className=''>
-        <div className='flex'>
-            <div className='p-2 bg-blue-muda'>
-                <img src={berita} className='w-[28px]' alt="" />
+        <div className='mb-0.5 flex items-center relative text-white font-semibold uppercase'>
+            <div className='p-2 bg-blue-muda-100 rounded-full border-2 absolute -top-2'>
+                <img src={berita} className='w-[18px]' alt="" />
             </div>
-            <p>Berita</p>
-            <div className='bg-white'>
-                <img src={briLiga} className='w-[34px]' alt="" />
+            <p className='w-1/2 bg-blue-muda-100 py-1 text-center text-xs'>Berita</p>
+            <div className='bg-white border absolute left-[138px]'>
+                <img src={briLiga} className='w-[28px]' alt="" />
             </div>
-            <p className='bg-blue-primary text-white font-bold uppercase text-xs'>Update market value</p>
+            <p className='w-1/2 bg-blue-primary text-xs pl-5 p-1'>Update market value</p>
         </div>
-        <div>
-            <div>
-                <img src='' alt="" />
+        <div className='flex items-center'>
+            <div className='w-1/2'>
+                <img src={updateMV} className='w-[167px]' alt="" />
             </div>
+            <p className='px-2 w-1/2'>Kenaikan dan Penurunan Yang Cukup Terasa</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,11 @@ export default {
       colors: {
         'blue-primary': 'rgb(0, 25, 63)',
         'blue-secondary': 'rgba(255, 255, 255, .2)',
+        'blue-muda' : '#1D75A3',
+        'blue-muda-100' : '#00ADED',
         'button-click' : '#5ca6ff;',
         'grey-nav' : 'rgb(233, 233, 233)',
-        'blue-muda' : '#1D75A3',
-        'gray-con' : '#F2F2F2' 
+        'gray-con' : '#F2F2F2' ,
       },
       fontFamily: {
         'oswald': ['Oswald', 'sans-serif']
