901bea1a17a026363c57efe508a64a08da2f8687
Denni <dennipes1@gmail.com>
2024-06-24 16:11:49 +0700

    fix error and make responsive

--- src/components/fragments/ListFooter.jsx
+++ src/components/fragments/ListFooter.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListItemFooter from '../elements/ListItemFooter';
 
-const ListFooter = ({ header, items }) => {
+const ListFooter = ({ header, items, className }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMdUp, setIsMdUp] = useState(window.innerWidth >= 768);
 
   const toggleSection = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMdUp(window.innerWidth >= 768);
+    };
+    
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className='relative my-5'>
-      <div className='flex justify-between items-center cursor-pointer' onClick={toggleSection}>
-        <h1 className='uppercase font-semibold'>{header}</h1>
+    <div className='relative my-5 md:m-0'>
+      <div className='flex justify-between items-center cursor-pointer md:cursor-auto' onClick={toggleSection}>
+        <h1 className={`uppercase font-semibold ${className}`}>{header}</h1>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth="2"
           stroke="white"
-          className={`w-3 h-3 transform ${isOpen ? 'rotate-180' : 'rotate-90'}`}
+          className={`w-3 h-3 transform transition-transform ${isOpen ? 'rotate-180' : 'rotate-90'} md:hidden`}
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 19l7-7-7-7" />
         </svg>
       </div>
-      <hr className='absolute -bottom-2 w-full border-blue-950 border' />
-      {isOpen && (
+      <hr className='md:hidden absolute -bottom-2 w-full border-blue-950 border' />
+      {(isOpen || isMdUp) && (
         <ul className='py-4'>
           {items.map((item, index) => (
             <ListItemFooter key={index} text={item} />
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -20,14 +20,13 @@ const Footer = () => {
 
   const careerItems = [
     'Karir 1',
-    'Karir 2',
-    'Karir 3'
   ];
 
   const involvementItems = [
     'Ikut terlibat 1',
     'Ikut terlibat 2',
-    'Ikut terlibat 3'
+    'Ikut terlibat 3',
+    'Daftar sebagai moderator atau data scout'
   ];
 
   const aboutUsItems = [
@@ -37,24 +36,28 @@ const Footer = () => {
   ];
 
   return (
-    <div className='md:flex justify-around bg-blue-primary text-white font-sans'>
-      <div className='px-10 pt-5 pb-7'>
-        <ListFooter header='Link cepat' items={quickLinksItems} />
-        <ListFooter header='Karir' items={careerItems} />
-        <ListFooter header='Ikut terlibat' items={involvementItems} />
-        <ListFooter header='Tentang kita' items={aboutUsItems} />
-      </div>
-      <div className='bg-blue-950 px-10 py-7'>
-        <h1 className='uppercase font-bold text-center mb-5'>Media Sosial</h1>
-        <div className='flex items-center justify-center flex-wrap gap-x-8 gap-y-5'>
-            <div><img src={facebook} className='w-[20px] h-[20px]'  alt="" /></div>
-            <div><img src={instagram} className='w-[20px]' alt="" /></div>
-            <div><img src={threads} className='w-[20px]' alt="" /></div>
-            <div><img src={tiktok} className='w-[20px]' alt="" /></div>
-            <div><img src={twitter} className='w-[20px]' alt="" /></div>
-            <div><img src={whatsapp} className='w-[20px]' alt="" /></div>
-            <div><img src={wifi} className='w-[20px]' alt="" /></div>
-            <div><img src={youtube} className='w-[20px]' alt="" /></div>
+    <div className='bg-blue-primary text-white font-sans md:pt-10'>
+      <div className='md:grid grid-cols-3 md:px-10'>
+        <div className='px-10 pt-5 md:p-0 '>
+          <ListFooter header='Link cepat' items={quickLinksItems} />
+          <ListFooter header='Karir' items={careerItems} />
+        </div>
+        <div className='px-10 pb-7 md:p-0'>
+          <ListFooter header='Ikut terlibat' items={involvementItems} />
+          <ListFooter header='Tentang kita' items={aboutUsItems} className='md:mt-5' />
+        </div>
+        <div className='bg-blue-950 px-10 py-7 md:bg-blue-primary md:p-0 md:pr-8'>
+          <h1 className='uppercase font-bold text-center mb-5 md:text-start'>Media Sosial</h1>
+          <div className='flex items-center justify-center flex-wrap gap-x-8 gap-y-5 md:justify-start'>
+              <div><img src={facebook} className='w-[20px] h-[20px]'  alt="" /></div>
+              <div><img src={instagram} className='w-[20px]' alt="" /></div>
+              <div><img src={threads} className='w-[20px]' alt="" /></div>
+              <div><img src={tiktok} className='w-[20px]' alt="" /></div>
+              <div><img src={twitter} className='w-[20px]' alt="" /></div>
+              <div><img src={whatsapp} className='w-[20px]' alt="" /></div>
+              <div><img src={wifi} className='w-[20px]' alt="" /></div>
+              <div><img src={youtube} className='w-[20px]' alt="" /></div>
+          </div>
         </div>
       </div>
     </div>
